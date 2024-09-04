ecd7f11ebc6b6596caa23ef2384affaedbefb914
Aleksey <black-colorss@mail.ru>
2024-09-04 20:03:25 +0300

    start

---
+++ src/main/java/com/app/expence/accounting/ExpenceAccountingApplication.java
@@ -0,0 +1,13 @@
+package com.app.expence.accounting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenceAccountingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpenceAccountingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/expence/accounting/ExpenceAccountingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.expence.accounting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenceAccountingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
